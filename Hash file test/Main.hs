47b5134d49ca6da9c184466841140ceeac4dfc16
Sarah-Scott <38771351+Sarah-Scott@users.noreply.github.com>
2018-08-30 15:14:09 -0500

    added signing kinda

--- Hash file test/Main.hs
+++ Hash file test/Main.hs
@@ -1,45 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-9.3 script
 
-
-
-
-{-
---hashes a file given statically using MD5
-
-
-import qualified Data.ByteString.Lazy as LB
-import Data.Digest.Pure.MD5
-
-main :: IO ()
-main = do
-    fileContent <- LB.readFile "test1.txt"
-    let md5Digest = md5 fileContent
-    print md5Digest
--}
-
-{-
---hashes a string given at runtime using SHA256
-
-import           Crypto.Hash             (hashWith, SHA256 (..))
-import           Data.ByteString         (ByteString)
-import           Data.Text.Encoding      (encodeUtf8)
-import qualified Data.Text.IO            as TIO
-import           System.IO               (hFlush, stdout)
-
-main :: IO ()
-main = do
-  putStr "Enter some text: "
-  hFlush stdout
-  text <- TIO.getLine
-  let bs = encodeUtf8 text
-  putStrLn $ "You entered: " ++ show bs
-  let digest = hashWith SHA256 bs
-  putStrLn $ "SHA256 hash: " ++ show digest
--}
-
-
-
 --getLine :: IO Text
 --encodeUtf8 :: Text -> ByteString
 --hashWith :: (ByteArrayAccess ba, HashAlgorithm alg) => alg -> ba -> Digest alg
@@ -47,7 +8,12 @@ main = do
 
 
 
+--do I have to call each of these main?
+--how do I have multiple functions at a time?
+--I don't understand this main/IO business
 
+
+{-
 --hashes a file given statically using SHA256
 
 import           Crypto.Hash             (hashWith, SHA256 (..))
@@ -59,3 +25,32 @@ main = do
   print fileContent
   let digest = hashWith SHA256 fileContent
   print digest
+-}
+
+{-
+--generates a nonce
+
+import qualified Crypto.Nonce as CN
+import qualified Data.ByteString as B
+
+main :: IO ()
+main = do
+  g <- CN.new
+  n <- CN.nonce128 g 
+  print n
+-}  
+
+
+--signs a file's contents then verifies it
+
+import Crypto.Sign.Ed25519
+import qualified Data.ByteString as B
+
+main :: IO ()
+main = do
+  (pk0,sk0) <- createKeypair
+  (pk1,sk1) <- createKeypair
+  fileContent <- B.readFile "test1.txt"
+  let msg = sign sk0 fileContent
+  print $ verify pk0 msg
+  print $ verify pk1 msg
