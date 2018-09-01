683620a2096e5aaf97ccdce2b7a3f53c09312454
Sarah-Scott <38771351+Sarah-Scott@users.noreply.github.com>
2018-08-31 22:12:49 -0500

    fixed function names

--- Hash file test/Main.hs
+++ Hash file test/Main.hs
@@ -1,56 +1,46 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-9.3 script
 
+
+import Crypto.Sign.Ed25519
+import qualified Data.ByteString as B
+import qualified Crypto.Nonce as CN
+import           Crypto.Hash             (hashWith, SHA256 (..))
+
 --getLine :: IO Text
 --encodeUtf8 :: Text -> ByteString
 --hashWith :: (ByteArrayAccess ba, HashAlgorithm alg) => alg -> ba -> Digest alg
 --readFile :: FilePath -> IO ByteString
 
 
-
---do I have to call each of these main?
---how do I have multiple functions at a time?
---I don't understand this main/IO business
-
-
-{-
 --hashes a file given statically using SHA256
-
-import           Crypto.Hash             (hashWith, SHA256 (..))
-import qualified Data.ByteString         as B
-
-main :: IO ()
-main = do
+doHash :: IO ()
+doHash = do
   fileContent <- B.readFile "testMultiLine.txt"
   print fileContent
   let digest = hashWith SHA256 fileContent
   print digest
--}
 
-{-
+
+
 --generates a nonce
-
-import qualified Crypto.Nonce as CN
-import qualified Data.ByteString as B
-
-main :: IO ()
-main = do
+doNonce :: IO ()
+doNonce = do
   g <- CN.new
   n <- CN.nonce128 g 
   print n
--}  
+--should this generate something with only numbers?
+ 
 
 
 --signs a file's contents then verifies it
-
-import Crypto.Sign.Ed25519
-import qualified Data.ByteString as B
-
-main :: IO ()
-main = do
+doSign :: IO ()
+doSign = do
   (pk0,sk0) <- createKeypair
   (pk1,sk1) <- createKeypair
   fileContent <- B.readFile "test1.txt"
   let msg = sign sk0 fileContent
   print $ verify pk0 msg
   print $ verify pk1 msg
+--what am I supposed to be signing??
+
